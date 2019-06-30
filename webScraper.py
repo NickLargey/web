2072c4658e1d8bdc41b89c6ee70ef0b476a1a547
debbie <debbie@debbie.debbie.com>
2019-06-30 19:08:32 -0400

    WIP

--- webScraper.py
+++ webScraper.py
@@ -1,5 +1,6 @@
 import requests as re
 from lxml import html
+from datetime import datetime
 
 class Scraper:
 	"""A web-scraper that looks up what show's are going on in 
@@ -12,16 +13,36 @@ class Scraper:
 		self.call = re.get(url)
 		# self.text = self.call.text /// test to make sure call worked (debug)
 
+	def get_dates_and_shows(self):
 		# use lxml library to get dates from html class tags
 		self.tree = html.fromstring(self.call.content)
 		# create a list of dates using xpath
 		self.month = self.tree.xpath('//span[@class="month"]/text()')
-		self.day = self.tree.xpath('//span[@class="date"]/text()') 
-		#create single list of date tuples from two html tags
-		self.dates = list(map(''.join,(zip(self.month,self.day))))
-			
+		self.day = self.tree.xpath('//span[@class="date"]/text()')
+		self.events = self.tree.xpath('//div[@class="event_title"]/text()')
+		self.year = str(datetime.now().year) 
+		# create single list of date strings from tuples of two html tags
+		self.dates = [s + self.year for s in list(map(''.join,(zip(self.month,self.day))))]
+		self.shows = list(map(' '.join,(zip(self.dates, self.events))))
+		#!!! add feature to roll year over between dec/jan
+		print(self.shows)
 
+	def get_dates_and_shows_facebok(self):
+		# use lxml library to get dates from html class tags
+		self.ftree = html.fromstring(self.call.content)
+		# create a list of dates using xpath
+		self.fdates = self.ftree.xpath('//time[@class="list-card__date"]/text()')
+		self.fevents = self.ftree.xpath('//div[@class="list-card__title"]/text()')
+		
+		self.shows = list(map(''.join,(zip(self.fdates, self.fevents))))
+		self.fshows = [i.replace('\n','').strip().split() for i in self.shows]
+		self.ffshows = list(map(' '.join, self.fshows)) 
+		# [i.replace('\n','').strip() for i in self.fshows]
+		print(self.ffshows)
+		
 
 apoha = Scraper('https://www.theapohadiontheater.com/')
-
-print(apoha.dates)
+sun_tiki = Scraper('https://www.facebook.com/pg/suntikistudios/events/')
+genos = Scraper('https://www.eventbrite.com/o/genos-rock-club-15681751194')
+print(apoha.get_dates_and_shows())
+print(genos.get_dates_and_shows_facebok())
