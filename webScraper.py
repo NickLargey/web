1deeb406893975ad47c0f3997259d94ae3e2b9d5
debbie <debbie@debbie.debbie.com>
2019-07-01 22:14:35 -0400

    added eventbrite function

--- webScraper.py
+++ webScraper.py
@@ -27,22 +27,34 @@ class Scraper:
 		#!!! add feature to roll year over between dec/jan
 		print(self.shows)
 
-	def get_dates_and_shows_facebok(self):
+	def get_eventbrite(self):
 		# use lxml library to get dates from html class tags
 		self.ftree = html.fromstring(self.call.content)
 		# create a list of dates using xpath
-		self.fdates = self.ftree.xpath('//time[@class="list-card__date"]/text()')
-		self.fevents = self.ftree.xpath('//div[@class="list-card__title"]/text()')
+		self.fdates = self.ftree.xpath('//article[@id="live_events"]//time[@class="list-card__date"]/text()')
+		self.fevents = self.ftree.xpath('//article[@id="live_events"]//div[@class="list-card__title"]/text()')
 		
+		# Ugly as sin, but only way I could figure out how to clean up eventbrite data.
 		self.shows = list(map(''.join,(zip(self.fdates, self.fevents))))
 		self.fshows = [i.replace('\n','').strip().split() for i in self.shows]
 		self.ffshows = list(map(' '.join, self.fshows)) 
-		# [i.replace('\n','').strip() for i in self.fshows]
-		print(self.ffshows)
 		
+		print(self.ffshows)
+
+	def get_songkick(self):
+		# use lxml library to get dates from html class tags
+		self.stree = html.fromstring(self.call.content)
+		# create a list of dates using xpath
+		self.sdates = self.stree.xpath('//li[@class="with-date"]/strong/time/text()')
+		self.sevents = self.stree.xpath('//p[@class="artists summary"]/a/span/strong/text()')
+		self.shows = list(map(' '.join,(zip(self.sdates, self.sevents))))
+		
+		print(self.shows)
 
 apoha = Scraper('https://www.theapohadiontheater.com/')
-sun_tiki = Scraper('https://www.facebook.com/pg/suntikistudios/events/')
+sun_tiki = Scraper('https://www.songkick.com/venues/3951109-sun-tiki-studios')
 genos = Scraper('https://www.eventbrite.com/o/genos-rock-club-15681751194')
+
 print(apoha.get_dates_and_shows())
-print(genos.get_dates_and_shows_facebok())
+print(genos.get_eventbrite())
+print(sun_tiki.get_songkick())
