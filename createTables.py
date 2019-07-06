88a85f5befefd1ac35dc239c10ec8a8f13369b22
NickLargey <Nick.Largey@gmail.com>
2019-07-05 23:19:13 -0400

    initial commit

---
+++ createTables.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+ 
+import psycopg2
+from config import config
+ 
+ 
+def create_tables():
+    """ create tables in the PostgreSQL database"""
+    commands = (
+        """
+        CREATE TABLE portland_shows (
+            date = datetime PRIMARY KEY,
+            Apohadion VARCHAR(50),
+            genos VARCHAR(50),
+            sun_tiki VARCHAR(500))
+            """
+        )
+    conn = None
+    try:
+        # read the connection parameters
+        params = config()
+        # connect to the PostgreSQL server
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+        # create table one by one
+        for command in commands:
+            cur.execute(command)
+        # close communication with the PostgreSQL database server
+        cur.close()
+        # commit the changes
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+ 
+ 
+if __name__ == '__main__':
+    create_tables()
